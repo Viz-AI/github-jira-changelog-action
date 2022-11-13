4a35d3321895e132be51157b22acb864acd97a24
elaz <elaz>
2022-11-13 20:25:24 +0200

    cherry pick changes to not change jira versions for now

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const core = require('@actions/core');
+const github = require('@actions/github');
 const _ = require('lodash');
 const Entities = require('html-entities');
 const ejs = require('ejs');
@@ -17,6 +18,8 @@ const config = {
     ticketIDPattern: RegExpFromString(core.getInput('jira_ticket_id_pattern')),
     approvalStatus: core.getInput('approval_statuses').split(",").filter(x => x !== ""),
     excludeIssueTypes: core.getInput('exclude_issue_types').split(",").filter(x => x !== ""),
+    approvalStatus: ['Done', 'Closed', 'Accepted'],
+    excludeIssueTypes: ['Sub-task'],
     includeIssueTypes: [],
   },
   sourceControl: {
@@ -30,49 +33,12 @@ const config = {
 
 
 const template = `
-<% if (jira.releaseVersions && jira.releaseVersions.length) {  %>
-Release version: <%= jira.releaseVersions[0].name -%>
-<% jira.releaseVersions.sort((a, b) => a.projectKey.localeCompare(b.projectKey)).forEach((release) => { %>
-  * [<%= release.projectKey %>](<%= jira.baseUrl + '/projects/' + release.projectKey + '/versions/' + release.id %>) <% -%>
-<% }); -%>
-<% } %>
-
-Jira Tickets
----------------------
 <% tickets.all.forEach((ticket) => { %>
   * [<%= ticket.fields.issuetype.name %>] - [<%= ticket.key %>](<%= jira.baseUrl + '/browse/' + ticket.key %>) <%= ticket.fields.summary -%>
 <% }); -%>
 <% if (!tickets.all.length) {%> ~ None ~ <% } %>
-
-Other Commits
----------------------
-<% commits.noTickets.forEach((commit) => { %>
-  * <%= commit.slackUser ? '@'+commit.slackUser.name : commit.authorName %> - [<%= commit.revision.substr(0, 7) %>] - <%= commit.summary -%>
-<% }); -%>
-<% if (!commits.noTickets.length) {%> ~ None ~ <% } %>
-
-<% if (includePendingApprovalSection) { %>
-Pending Approval
----------------------
-<% tickets.pendingByOwner.forEach((owner) => { %>
-<%= (owner.slackUser) ? '@'+owner.slackUser.name : owner.email %>
-<% owner.tickets.forEach((ticket) => { -%>
-  * <%= jira.baseUrl + '/browse/' + ticket.key %>
-<% }); -%>
-<% }); -%>
-<% if (!tickets.pendingByOwner.length) {%> ~ None. Yay! ~ <% } %>
-<% } %>
 `;
 
-function generateReleaseVersionName() {
-  const hasVersion = process.env.VERSION;
-  if (hasVersion) {
-    return process.env.VERSION;
-  } else {
-    const haikunator = new Haikunator();
-    return haikunator.haikunate();
-  }
-}
 
 function transformCommitLogs(config, logs) {
   let approvalStatus = config.jira.approvalStatus;
@@ -134,32 +100,19 @@ async function main() {
     const range = config.sourceControl.defaultRange;
     console.log(`Getting range ${range.from}...${range.to} commit logs`);
     const commitLogs = await source.getCommitLogs('./', range);
-    console.log('Found following commit logs:');
-    console.log(commitLogs);
-
-    console.log('Generating release version');
-    const release = generateReleaseVersionName();
-    console.log(`Release: ${release}`);
 
     console.log('Generating Jira changelog from commit logs');
-    const changelog = await jira.generate(commitLogs, release);
-    console.log('Changelog entry:');
-    console.log(changelog);
+    const changelog = await jira.generate(commitLogs);
 
     console.log('Generating changelog message');
     const data = await transformCommitLogs(config, changelog);
 
     data.jira = {
       baseUrl: config.jira.baseUrl,
-      releaseVersions: jira.releaseVersions,
     };
     data.includePendingApprovalSection = core.getInput('include_pending_approval_section') === 'true';
 
-    const entitles = new Entities.AllHtmlEntities();
     const changelogMessage = ejs.render(template, data);
-
-    console.log('Changelog message entry:');
-    console.log(entitles.decode(changelogMessage));
 
     core.setOutput('changelog_message', changelogMessage);
 
